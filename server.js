621d36e882a1b27b5986bc4a92be81e172f0078c
sirnobys <sirnobys@gmail.com>
2022-05-06 12:34:08 +0000

    test node

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
-
-var con = mysql.createConnection({
+db_config ={
   host: "us-cdbr-east-05.cleardb.net",
   user: "b8d01d88271309",
   password: "3b220325",
   database: 'heroku_9390bfdc44d4566'
-});
+}
+var con = mysql.createConnection(db_config);
 con.connect(function (err) {
   if (err) {
     return console.error('error: ' + err.message);
@@ -32,6 +32,31 @@ let messages = []
 let block = []
 let users = []
 let active_users = {}
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
+  
 app.use('/', (req, res) => {
   con.query("SELECT * FROM user", (err, result, fields) => {
     if (err) throw err;
@@ -54,14 +79,17 @@ io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
   socket.on("activate_user", (data) => {
+    
     active_users[data['email']] = data
     socket.emit("user_activated", data)
     let sql = 'SELECT * FROM user WHERE email = ?';
     let result =[]
+
     con.query(sql, [data['email']],  (err, result)=> {
       if (err) throw err;
       result = result
     });
+
     if(result.length<1){
       sql = 'INSERT INTO user (name, email, picture) VALUES (?,?,?)';
       con.query(sql, [data['name'],data['email'],data['picture']],  (err, result)=> {
