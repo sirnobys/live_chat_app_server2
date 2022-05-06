98e53c6fbc8fcfa3bb6dfb889b7497985d255364
sirnobys <sirnobys@gmail.com>
2022-05-06 11:46:36 +0000

    test node

--- server.js
+++ server.js
@@ -21,39 +21,54 @@ var con = mysql.createConnection({
   password: "3b220325",
   database: 'heroku_9390bfdc44d4566'
 });
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) {
     return console.error('error: ' + err.message);
   }
   console.log('Connected to the MySQL server.');
 });
-let messages=[]
-let block=[]
-let users=[]
-app.use('/',(req,res)=>{
-  con.query("SELECT * FROM user",  (err, result, fields)=> {
+
+let messages = []
+let block = []
+let users = []
+let active_users = {}
+app.use('/', (req, res) => {
+  con.query("SELECT * FROM user", (err, result, fields) => {
     if (err) throw err;
-   users = result
+    users = result
   });
 
-  con.query("SELECT * FROM block",  (err, result, fields)=> {
+  con.query("SELECT * FROM block", (err, result, fields) => {
     if (err) throw err;
     block = result
   });
 
-  con.query("SELECT * FROM message",  (err, result, fields)=> {
+  con.query("SELECT * FROM message", (err, result, fields) => {
     if (err) throw err;
     messages = result
   });
-  res.send(JSON.stringify({data:{messages:messages, block:block, users:users }}))
+  res.send(JSON.stringify({ data: { messages: messages, block: block, users: users } }))
 })
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
   socket.on("activate_user", (data) => {
-    socket.emit("user_activated",data)
-    console.log(data);
+    active_users[data['email']] = data
+    socket.emit("user_activated", data)
+    let sql = 'SELECT * FROM user WHERE email = ?';
+    let result =[]
+    con.query(sql, [data['email']],  (err, result)=> {
+      if (err) throw err;
+      result = result
+    });
+    if(result.length<1){
+      sql = 'INSERT INTO user (name, email, picture) VALUES (?,?,?)';
+      con.query(sql, [data['name'],data['email'],data['picture']],  (err, result)=> {
+        if (err) throw err;
+        console.log('inserted succesfully');
+      });
+    }
   });
 
   socket.on("send_message", (data) => {
