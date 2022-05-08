2f9444098329a7368f01cff1dcdb9933b1316c2e
sirnobys <sirnobys@gmail.com>
2022-05-08 14:00:26 +0000

    config

--- server.js
+++ server.js
@@ -55,29 +55,58 @@ app.use('/', (req, res) => {
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
-
+  socket.on("fetch", () => {
+    fetch()
+    socket.emit('fetched',{messages, block, users})
+  })
   socket.on("activate_user", (data) => {
     
     active_users[data['email']] = data
-    socket.emit("user_activated", data)
+    socket.emit("user_activated", active_users)
     let sql = 'SELECT * FROM user WHERE email = ?';
-    let result =[]
-
     con.query(sql, [data['email']],  (err, result)=> {
       if (err) throw err;
       if(result.length<1){
         sql = 'INSERT INTO user (name, email, picture) VALUES (?,?,?)';
         con.query(sql, [data['name'],data['email'],data['picture']],  (err, result)=> {
           if (err) throw err;
-          console.log('inserted succesfully');
+          console.log('inserted successfully');
         });
       }
     });
-
   });
 
   socket.on("send_message", (data) => {
-    socket.emit("receive_message", data);
+    messages.push(data)
+    socket.emit("message_sent", messages);
+    sql = 'INSERT INTO message (room, sender, receiver,time,message) VALUES (?,?,?,?,?)';
+        con.query(sql, [data['room'], data['sender'], data['receiver'], data['sent'], data['message']],  (err, result)=> {
+          if (err) throw err;
+          console.log('inserted message successfully');
+        });
+  });
+
+  socket.on("block_user", (data) => {
+    socket.emit('user_blocked',data)
+    sql = 'INSERT into block (blocked_user, user) value (?,?)';
+        con.query(sql, [data['blocked_user'], data['user']],  (err, result)=> {
+          if (err) throw err;
+          console.log('inserted block successfully');
+        });
+  });
+
+  socket.on("unblock_user", (data) => {
+    socket.emit('user_unblocked',data)
+    sql = 'DELETE FROM block WHERE blocked_user=? AND user=?';
+        con.query(sql, [data['blocked_user'], data['user']],  (err, result)=> {
+          if (err) throw err;
+          console.log('deleted block successfully');
+        });
+  });
+
+  socket.on("deactivate_user", (data) => {
+    delete active_users[data["emeail"]]
+    socket.emit('user_deactivated',data)
   });
 
   socket.on("disconnect", () => {
