6cb89babaacee1f8a8bb7e54adeda8958ae26e5f
sirnobys <sirnobys@gmail.com>
2022-05-08 13:55:34 +0000

    config

--- server.js
+++ server.js
@@ -33,28 +33,6 @@ let block = []
 let users = []
 let active_users = {}
 
-// function handleDisconnect() {
-//   connection = mysql.createConnection(db_config); // Recreate the connection, since
-//                                                   // the old one cannot be reused.
-
-//   connection.connect(function(err) {              // The server is either down
-//     if(err) {                                     // or restarting (takes a while sometimes).
-//       console.log('error when connecting to db:', err);
-//       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-//     }                                     // to avoid a hot loop, and to allow our node script to
-//   });                                     // process asynchronous requests in the meantime.
-//                                           // If you're also serving http, display a 503 error.
-//   connection.on('error', function(err) {
-//     console.log('db error', err);
-//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-//       handleDisconnect();                         // lost due to either server restart, or a
-//     } else {                                      // connnection idle timeout (the wait_timeout
-//       throw err;                                  // server variable configures this)
-//     }
-//   });
-// }
-
-// handleDisconnect();
 
   
 app.use('/', (req, res) => {
@@ -87,16 +65,15 @@ io.on("connection", (socket) => {
 
     con.query(sql, [data['email']],  (err, result)=> {
       if (err) throw err;
-      result = result
+      if(result.length<1){
+        sql = 'INSERT INTO user (name, email, picture) VALUES (?,?,?)';
+        con.query(sql, [data['name'],data['email'],data['picture']],  (err, result)=> {
+          if (err) throw err;
+          console.log('inserted succesfully');
+        });
+      }
     });
 
-    if(result.length<1){
-      sql = 'INSERT INTO user (name, email, picture) VALUES (?,?,?)';
-      con.query(sql, [data['name'],data['email'],data['picture']],  (err, result)=> {
-        if (err) throw err;
-        console.log('inserted succesfully');
-      });
-    }
   });
 
   socket.on("send_message", (data) => {
