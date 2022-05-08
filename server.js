42cf60ce2bdb26ca654ff6688525a4642ed67a9e
sirnobys <sirnobys@gmail.com>
2022-05-08 17:29:19 +0000

    config

--- server.js
+++ server.js
@@ -108,11 +108,10 @@ io.on("connection", (socket) => {
   });
 
   socket.on("block_user", (data) => {
-    socket.emit('user_blocked', data)
     if(!(block.some(e=>e.user==data.user && e.blocked_user == data.blocked_user))){
       block.push(data)
     }
-    socket.emit('fetched', messages,block,users)
+    socket.emit('user_blocked', block)
     let sql = "SELECT * FROM block WHERE blocked_user=? AND user=?"
     con.query(sql, [data['blocked_user'], data['user']], (err, result) => {
       if (err) throw err;
@@ -129,8 +128,7 @@ io.on("connection", (socket) => {
 
   socket.on("unblock_user", (data) => {
     block = block.filter(e=>!(e.user==data.user && e.blocked_user == data.blocked_user))
-    socket.emit('user_unblocked', data)
-    socket.emit('fetched', messages,block,users)
+    socket.emit('user_unblocked', block)
     let sql = 'DELETE FROM block WHERE blocked_user=? AND user=?';
     con.query(sql, [data['blocked_user'], data['user']], (err, result) => {
       if (err) throw err;
